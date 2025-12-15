1428107f850c9068b9403aaa80214af33bcdf64f
siddharthchary <siddharthchary20@gmail.com>
2025-12-15 14:30:31 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scrolling
+document.querySelectorAll("nav a").forEach(link => {
+  link.addEventListener("click", event => {
+    event.preventDefault();
+    document.querySelector(link.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
